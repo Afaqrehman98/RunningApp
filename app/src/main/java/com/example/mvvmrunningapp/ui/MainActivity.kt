ab517558646044e7410bd84926ea40c19a24ccae
afaqrehman98 <afaqrehman98@gmail.com>
2021-06-02 03:26:12 +0500

    added maps, permissions, services

--- app/src/main/java/com/example/mvvmrunningapp/ui/MainActivity.kt
+++ app/src/main/java/com/example/mvvmrunningapp/ui/MainActivity.kt
@@ -1,14 +1,17 @@
 package com.example.mvvmrunningapp.ui
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.View
-import androidx.navigation.fragment.NavHostFragment.findNavController
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
 import com.example.mvvmrunningapp.R
 import com.example.mvvmrunningapp.databinding.ActivityMainBinding
+import dagger.hilt.android.AndroidEntryPoint
 
+
+@AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
     private lateinit var activityMainBinding: ActivityMainBinding
 
@@ -17,19 +20,23 @@ class MainActivity : AppCompatActivity() {
         activityMainBinding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(activityMainBinding.root)
 
-//        val navController = findNavController(R.id.nav_host_fragment)
-//
-//        setSupportActionBar(activityMainBinding.toolbar)
-//        activityMainBinding.bottomNavigationView.setupWithNavController(activityMainBinding.navHostFragment.findNavController())
-//
-//        val navHost = FindVi
-//        activityMainBinding.navHostFragment.findNavController()
-//            .addOnDestinationChangedListener { _, destination, _ ->
-//                when (destination.id) {
-//                    R.id.settingsFragment, R.id.runFragment, R.id.statisticsFragment ->
-//                        bottomNavigationView.visibility = View.VISIBLE
-//                    else -> bottomNavigationView.visibility = View.GONE
-//                }
-//            }
+        setSupportActionBar(activityMainBinding.toolbar)
+
+        val bottomNavigation = activityMainBinding.bottomNavigationView
+        val navController = findNavController(R.id.nav_host_fragment_container)
+
+        bottomNavigation.setupWithNavController(navController)
+
+        val appBarConfiguration =
+            AppBarConfiguration(
+                setOf(
+                    R.id.settingsFragment,
+                    R.id.runFragment,
+                    R.id.statisticsFragment
+                )
+            )
+
+        setupActionBarWithNavController(navController, appBarConfiguration)
+
     }
 }
--- app/src/main/java/com/example/mvvmrunningapp/ui/fragments/RunFragment.kt
+++ app/src/main/java/com/example/mvvmrunningapp/ui/fragments/RunFragment.kt
@@ -1,13 +1,88 @@
 package com.example.mvvmrunningapp.ui.fragments
 
+import android.os.Build
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
+import androidx.navigation.fragment.findNavController
 import com.example.mvvmrunningapp.R
+import com.example.mvvmrunningapp.databinding.FragmentRunBinding
+import com.example.mvvmrunningapp.others.Constants.REQUEST_CODE_LOCATION_PERMISSION
+import com.example.mvvmrunningapp.others.TrackingUtility
 import com.example.mvvmrunningapp.ui.viewmodels.MainViewModel
 import dagger.hilt.android.AndroidEntryPoint
+import pub.devrel.easypermissions.AppSettingsDialog
+import pub.devrel.easypermissions.EasyPermissions
 
 @AndroidEntryPoint
-class RunFragment : Fragment(R.layout.fragment_run) {
-
+class RunFragment : Fragment(), EasyPermissions.PermissionCallbacks {
+    private lateinit var binding: FragmentRunBinding
     private val viewModel: MainViewModel by viewModels()
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = FragmentRunBinding.inflate(layoutInflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        requestPermission()
+
+        binding.fab.setOnClickListener {
+            findNavController().navigate(R.id.action_runFragment_to_trackingFragment)
+        }
+    }
+
+    private fun requestPermission() {
+        if (TrackingUtility.hasLocationPermission(requireContext())) {
+            return
+        }
+        if (Build.VERSION.SDK_INT < Build.VERSION_CODES.Q) {
+            EasyPermissions.requestPermissions(
+                this,
+                "You need to accept permission to use this app",
+                REQUEST_CODE_LOCATION_PERMISSION,
+                android.Manifest.permission.ACCESS_FINE_LOCATION,
+                android.Manifest.permission.ACCESS_COARSE_LOCATION
+
+            )
+        } else {
+            EasyPermissions.requestPermissions(
+                this,
+                "You need to accept permission to use this app",
+                REQUEST_CODE_LOCATION_PERMISSION,
+                android.Manifest.permission.ACCESS_FINE_LOCATION,
+                android.Manifest.permission.ACCESS_COARSE_LOCATION,
+                android.Manifest.permission.ACCESS_BACKGROUND_LOCATION
+            )
+        }
+    }
+
+    override fun onPermissionsGranted(requestCode: Int, perms: MutableList<String>) {
+        if (EasyPermissions.somePermissionPermanentlyDenied(this, perms)) {
+            AppSettingsDialog.Builder(this).build().show()
+        } else {
+            requestPermission()
+        }
+    }
+
+    override fun onPermissionsDenied(requestCode: Int, perms: MutableList<String>) {
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        EasyPermissions.onRequestPermissionsResult(requestCode, permissions, grantResults, this)
+    }
 }
